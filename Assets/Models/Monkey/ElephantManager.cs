d2577eca8d161682c997f18b3259adf1c6e3e205
DanylUnity3D <devandrienko@gmail.com>
2017-12-18 14:59:16 +0200

    Upload

---
+++ Assets/Models/Monkey/ElephantManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ElephantManager : MonoBehaviour
+{
+    [SerializeField] private GameObject monkey;
+    [SerializeField] private Animator monkeyAnimator;
+    //[SerializeField] private AudioSource monkeyAudioSource;
+
+    [SerializeField] private GameObject buttonPlay;
+    [SerializeField] private GameObject buttonPause;
+    [SerializeField] private GameObject buttonStop;
+
+    public void Play()
+    {
+        //monkeyAudioSource.Play();
+        monkeyAnimator.enabled = true;
+        buttonPlay.SetActive(false);
+        buttonPause.SetActive(true);
+    }
+
+    public void Pause()
+    {
+        monkeyAnimator.enabled = false;
+        //monkeyAudioSource.Pause();
+        buttonPause.SetActive(false);
+        buttonPlay.SetActive(true);
+    }
+
+    public void Stop()
+    {
+        monkey.SetActive(false);
+        monkey.SetActive(true);
+        monkeyAnimator.enabled = false;
+        //monkeyAudioSource.Stop();
+        buttonPause.SetActive(false);
+        buttonPlay.SetActive(true);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ColoringAR
-AR Coloring project
+# ColoringAR [In Dev]
+AR Coloring project with different characters, such as:
+- Cute duck
+- Girl in garden, with parrot
+- Dancing monkey
+- Wild elephant
+
+When you place phone camera on coloring - you will see 3D environment with appropriate character
+# Download Builds
+Link: https://goo.gl/wMe7uP
